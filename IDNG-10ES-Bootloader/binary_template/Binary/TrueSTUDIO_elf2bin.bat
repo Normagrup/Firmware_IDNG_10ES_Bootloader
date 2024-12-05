5be33061b624c57d70223a4aaef35cf5ae8da885
Ivan Garcia Hernandez <ivangarhez98@gmail.com>
2024-12-05 09:03:45 +0100

    remove extra files

--- IDNG-10ES-Bootloader/binary_template/Binary/TrueSTUDIO_elf2bin.bat
+++
@@ -1,4 +0,0 @@
-@if exist .\..\TrueSTUDIO\STM320518_EVAL\Debug\STM320518_EVAL.elf ("arm-elf-objcopy.exe" -O binary ".\..\TrueSTUDIO\STM320518_EVAL\Debug\STM320518_EVAL.elf" "STM320518_EVAL_SysTick.bin")
-
-pause
-
